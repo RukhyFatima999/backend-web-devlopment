c87821878f2246148102830bb7d17e1595bc5bc0
RukhyFatima999 <anayamalikkhan999@gmail.com>
2023-10-16 14:38:06 +0500

    enumerate

---
+++ enumerate.py
@@ -0,0 +1,8 @@
+a=["eat","sleep","repeat"]
+b="geek"
+obj1=enumerate(a)
+obj2=enumerate(b)
+print("return type:" , type(obj1))
+print(list(enumerate(a)))
+print(list(enumerate(b,2)))
+
--- tuple.py
+++ tuple.py
@@ -3,6 +3,5 @@ def convert_sec(seconds):
  minutes=(seconds- hours * 3600) // 60
  remainind_sec=seconds - hours * 3600 - minutes * 60
  return hours, minutes, remainind_sec
-result = convert_sec(5000)
-print (result)
-
+hours,minutes,seconds = convert_sec(1000)
+print (hours, minutes,seconds)
