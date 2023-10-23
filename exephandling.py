ee1d39353559c22b4064a1feef6da99494d1d89a
RukhyFatima999 <anayamalikkhan999@gmail.com>
2023-10-23 15:32:31 +0500

    tryexecpt

--- exephandling.py
+++ exephandling.py
@@ -1,6 +1,6 @@
 def divide_by(a,b):
     return a/b
 try:
-     ans=divide_by(40,4)
-except:
-   print("something went wrong!") 
+    ans=divide_by(40,4)
+except Exception as e:
+   print("something went wrong!",e) 
